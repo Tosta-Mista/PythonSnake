0ab15c23dca3b78ec88705b56ec06d706f71b384
Jouz <rouletamix@gmail.com>
2014-01-05 05:57:32 +0100

    Initial commit

---
+++ PySnake.py
@@ -0,0 +1,13 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# PySnake
+# A simple snake game using pygame lib
+# Author : José Gonçalves
+
+import pygame, sys, time, random
+from pygame.locals import *
+
+
+pygame.init()
+fpsClock = pygame.time.Clock()
