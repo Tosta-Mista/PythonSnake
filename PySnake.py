7c935673e9a61bca63f8507006f06892d6f219f8
Jouz <rouletamix@gmail.com>
2014-01-05 06:07:12 +0100

    Set game settings, define color and initiate some var.

--- PySnake.py
+++ PySnake.py
@@ -10,4 +10,21 @@ from pygame.locals import *
 
 
 pygame.init()
+
+#Game settings :
 fpsClock = pygame.time.Clock()
+playSurface = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('PySnake')
+
+## Define some colors :
+redColour = pygame.Color(255, 0, 0)
+blackColour = pygame.Color(0, 0, 0)
+whiteColour = pygame.Color(255, 255, 255)
+greyColour = pygame.Color(150, 150, 150)
+
+snakePosition = [100, 100]
+snakeSegments = [[100,100], [80, 100], [60,100]]
+fruitPosition = [300, 300]
+fruitSpawned = 1
+direction = 'right'
+changeDirection = direction
